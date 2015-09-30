3e1d3560b0fb723c601e68101d48a10748fa34e1
Matthew Madurski <matt@matthewmadurski.com>
2015-09-30 08:24:10 -0400

    more validators

--- scanner/validators.py
+++ scanner/validators.py
@@ -1,3 +1,6 @@
+import csv
+
+
 def validate_config(config):
 	for key, value in config.iteritems():
 		assert isinstance(key, int)
@@ -5,3 +8,9 @@ def validate_config(config):
 		assert 'value' in value
 		assert 'condition' in value
 		assert hasattr(value['condition'], '__call__')
+
+
+def config_matches_csv(config, csv_file):
+	with open(csv_file, 'r') as f:
+		row = csv.reader(f).next()
+		assert len(row) >= max(config)
